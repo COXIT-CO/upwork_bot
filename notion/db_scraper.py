135af3ceeb0dfe3ef154c1557dca80012c16a24b
sviddo <snisarvlad04@gmail.com>
2023-01-10 18:57:09 +0200

    update notion db scraper script:
    - add main function leading the whole process from getting notion page url to return the extracted projects links
    - add separate validator of database id

--- notion/db_scraper.py
+++ notion/db_scraper.py
@@ -1,8 +1,19 @@
-from notion_database.database import Database
 import dotenv
 import os
+import validators
+from notion_database.database import Database
 
 dotenv.load_dotenv(".env")
+
+
+def run(notion_page_url: str):
+    """whole process from receiving link to notion page with database
+    to return the extracted projects links in database"""
+    is_url_valid = validators.url(notion_page_url)
+    if is_url_valid:
+        database_id = get_database_id_from_url(notion_page_url)
+        if is_database_id_valid(database_id):
+            return get_projects_urls(database_id)
 
 
 def get_projects_urls(database_id: str):
@@ -22,3 +33,10 @@ def get_database_id_from_url(url: str):
     # if database id contains hyphens capture first 36 letters else 32
     database_id = url_ending[:36] if "-" in url_ending[:36] else url_ending[:32]
     return database_id
+
+
+def is_database_id_valid(database_id: str):
+    for letter in database_id:
+        if not (letter.isalnum() or letter.isalpha() or letter == "-"):
+            return False
+    return True
