04c38a0038bdd1acd77b23de79d53efe08899452
sviddo <snisarvlad04@gmail.com>
2023-01-10 20:44:51 +0200

    add test for function capturing database id from utl

--- tests/notion_test.py
+++ tests/notion_test.py
@@ -1,5 +1,5 @@
 import pytest
-from notion.db_scraper import is_database_id_valid
+from notion.db_scraper import is_database_id_valid, get_database_id_from_url
 
 
 class TestDatabaseId:
@@ -23,3 +23,29 @@ class TestDatabaseId:
     )
     def test_invalid_database_id(self, test):
         assert not is_database_id_valid(test)
+
+
+@pytest.mark.parametrize(
+    "test",
+    [
+        "https://www.notion.so/6e0bdc477c8f4160a447ncb1f82218b3?v=d827154deef4430ba75fc82913e1d757",
+        "https://www.notion.so/6e0b-dc47-7c8f41-60a447n-cb1f82218b3?v=d827154deef4430ba75fc82913e1d757",
+        "this is invalid string, return it",
+        "",
+    ],
+)
+def test_get_database_id_from_url(test):
+    if (
+        test
+        == "https://www.notion.so/6e0bdc477c8f4160a447ncb1f82218b3?v=d827154deef4430ba75fc82913e1d757"
+    ):
+        assert get_database_id_from_url(test) == "6e0bdc477c8f4160a447ncb1f82218b3"
+    elif (
+        test
+        == "https://www.notion.so/6e0b-dc47-7c8f41-60a447n-cb1f82218b3?v=d827154deef4430ba75fc82913e1d757"
+    ):
+        assert get_database_id_from_url(test) == "6e0b-dc47-7c8f41-60a447n-cb1f82218b3"
+    elif test == "this is invalid string that have to be just returned":
+        assert get_database_id_from_url(test) == "this is invalid string, return it"
+    elif test == "":
+        assert get_database_id_from_url(test) == ""
