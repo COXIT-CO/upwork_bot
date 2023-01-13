bb70d932a8980647a53daafc3250e47258ffd5ea
sviddo <snisarvlad04@gmail.com>
2023-01-13 21:41:13 +0200

    add slack schema models

---
+++ slack/schema/models.py
@@ -0,0 +1,41 @@
+from flask_sqlalchemy import SQLAlchemy
+
+DB = SQLAlchemy()
+
+
+class Client(DB.Model):
+
+    __tablename__ = "client"
+
+    id = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(100), unique=True)
+    url = DB.Column(DB.String(200), unique=True)
+    client = DB.relationship(
+        "Job", backref="owner", cascade="delete, merge, save-update"
+    )
+    # one to many (name -> urls)
+
+    def save(self):
+        DB.session.add(self)
+        DB.session.commit()
+
+    def delete(self):
+        DB.session.delete(self)
+        DB.session.commit()
+
+
+class Job(DB.Model):
+
+    __tablename__ = "job"
+
+    id = DB.Column(DB.Integer, primary_key=True)
+    client_id = DB.Column(DB.Integer, DB.ForeignKey("client.id"))
+    clients_url = DB.Column(DB.String(200))
+
+    def save(self):
+        DB.session.add(self)
+        DB.session.commit()
+
+    def delete(self):
+        DB.session.delete(self)
+        DB.session.commit()
