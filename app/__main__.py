cd3b48c2270a4d4f4f0c0467216cc5f4add4a38a
sviddo <snisarvlad04@gmail.com>
2023-01-31 14:31:15 +0200

    add args parser to set them in time command is run

--- app/__main__.py
+++ app/__main__.py
@@ -1,7 +1,16 @@
 if __name__ == "__main__":
+    
     import dotenv
+    from . import args_parser
 
     dotenv.load_dotenv(".env")
+    dotenv.set_key(".env", "CLIENT_ID", args_parser.args.client_id)
+    dotenv.set_key(".env", "CLIENT_SECRET", args_parser.args.client_secret)
+    dotenv.set_key(".env", "REFRESH_TOKEN", args_parser.args.refresh_token)
+    dotenv.set_key(".env", "SLACK_BOT_TOKEN", args_parser.args.slack_bot_token)
+    dotenv.set_key(".env", "SLACK_SIGNING_SECRET", args_parser.args.slack_signing_secret)
+    dotenv.set_key(".env", "NOTION_TOKEN", args_parser.args.notion_token)
+    
     # import flask app and initialize routes to accept requests
     from .routes import flask_app
     from upwork_part.schema.models import Job
