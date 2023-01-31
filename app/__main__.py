0de12952c6836928cea4de92795c740e73df08b9
sviddo <snisarvlad04@gmail.com>
2023-01-31 17:44:04 +0200

    update main file launching the app

--- app/__main__.py
+++ app/__main__.py
@@ -3,14 +3,21 @@ if __name__ == "__main__":
     import dotenv
     from . import args_parser
 
-    dotenv.load_dotenv(".env")
+    # print(args_parser.args.client_id)
+    # print(args_parser.args.client_secret)
+    # print(args_parser.args.refresh_token)
+    # print(args_parser.args.slack_bot_token)
+    # print(args_parser.args.slack_signing_secret)
+    # print(args_parser.args.notion_token)
+
     dotenv.set_key(".env", "CLIENT_ID", args_parser.args.client_id)
     dotenv.set_key(".env", "CLIENT_SECRET", args_parser.args.client_secret)
     dotenv.set_key(".env", "REFRESH_TOKEN", args_parser.args.refresh_token)
     dotenv.set_key(".env", "SLACK_BOT_TOKEN", args_parser.args.slack_bot_token)
     dotenv.set_key(".env", "SLACK_SIGNING_SECRET", args_parser.args.slack_signing_secret)
     dotenv.set_key(".env", "NOTION_TOKEN", args_parser.args.notion_token)
-    
+    dotenv.load_dotenv(".env")
+
     # import flask app and initialize routes to accept requests
     from .routes import flask_app
     from upwork_part.schema.models import Job
