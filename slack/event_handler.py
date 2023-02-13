423fb6109d3a5699dfaddd17086a40061f57703d
volodymyr hresko <volodymyr.hresko@coxit.co>
2023-02-13 17:26:30 +0200

    update event handler

--- slack/event_handler.py
+++ slack/event_handler.py
@@ -1,112 +1,32 @@
 import ast
-
-# import dotenv
 import os
 import subprocess
-import threading
-from app.app import flask_app
-from helpers import exceptions
-from notion.notion_table_scraper import scrape_notion_table
 from slack.app import slack_bot_app
 from slack.utils import build_blocks_given_job_openings, build_blocks_given_invitations
-from upwork_part.schema.controllers import JobController
-from upwork_part.upwork_integration import Job
-from upwork_part.upwork_integration import upwork_client
-
-
-def handle_subscription(notion_table_url):
-    """event handler to user subscription"""
-    try:
-        projects_data = scrape_notion_table(notion_table_url)
-    except exceptions.CustomException as exc:
-        slack_bot_app.client.chat_postMessage(
-            channel=os.getenv("SLACK_CHANNEL_ID"), text=str(exc)
-        )
-        return ""
-    setup_cron_jobs()
-    jobs = []
-    for job_data in projects_data:
-        job_url = job_data["url"]
-        job_title = job_data["title"]
-        try:
-            job = Job(job_url).get_job(upwork_client.receive_upwork_client())
-            serialized_job_info = job.serialize_job()
-            save_jobs_to_db(serialized_job_info)
-            serialized_job_info["job_title"] = job_title
-            jobs.append(serialized_job_info)
-        except exceptions.CustomException as exc:
-            slack_bot_app.client.chat_postMessage(
-                channel=os.getenv("CHANNEL_ID"),
-                text=str(exc),
-            )
-    # dotenv.set_key(".env", "JOBS", str(jobs))
-    os.environ["JOBS"] = str(jobs)
-    slack_bot_app.client.chat_postMessage(
-        channel=os.getenv("SLACK_CHANNEL_ID"),
-        blocks=[
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Hi! I have new job openings for you :eyes:",
-                },
-            },
-            {
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "action_id": "modal_window_handler",
-                        "text": {"type": "plain_text", "text": "Watch them"},
-                        "style": "primary",
-                    }
-                ],
-            },
-        ],
-    )
-
-
-def save_jobs_to_db(serialized_job_data):
-    job_controller = JobController()
-    job_url = serialized_job_data["job_url"]
-    with flask_app.app_context():
-        job_controller.create(job_url)
-        for job_data in serialized_job_data["other_opened_jobs"]:
-            job_controller.create(job_data["job_url"])
-
-
-def setup_cron_jobs(level_up_directory_path):
-    # # subprocess.Popen(["crontab", "-r"])
-    # job_openings_cron_job = (
-    #     f"python {level_up_directory_path}/cron-jobs/cron_job_openings.py"
-    # )
-    # cron_jobs = f"* * * * * {job_openings_cron_job}\n"
-    # subprocess.Popen(f"echo 'abc' > abc.txt", shell=True)  # {level_up_directory_path}/crontab")
-    subprocess.Popen(f'echo "* * * * * root python {level_up_directory_path}/cron-jobs/test.py\n\
-# Don\'t remove the empty line at the end of this file. It is required to run the cron job" > /etc/cron.d/hello-cron', shell=True)
-    subprocess.Popen("crontab -f", shell=True)
-#     subprocess.Popen(f'echo "* * * * * root python {level_up_directory_path}/cron-jobs/cron_job_openings.py\n\
-# # Don\'t remove the empty line at the end of this file. It is required to run the cron job" > /etc/cron.d/hello-cron', shell=True)
-#     subprocess.Popen("cron -f", shell=True)
-    # # subprocess.Popen(f"echo '{cron_jobs}' | crontab -", shell=True)
 
 
 @slack_bot_app.command("/subscribe")
 def subscribe(ack, body):
     channel_id = body["channel_id"]
-    notion_table_url = body["text"]  # extract notion table url provided by user
-    # os.environ["SLACK_CHANNEL_ID"] = channel_id
-    # os.environ["NOTION_TABLE_URL"] = notion_table_url
-    # dotenv.set_key(".env", "SLACK_CHANNEL_ID", channel_id)
-    # dotenv.set_key(".env", "NOTION_TABLE_URL", notion_table_url)
-    # os.environ["SLACK_CHANNEL_ID"] = channel_id
-    # os.environ["NOTION_TABLE_URL"] = notion_table_url
+    notion_table_url = body["text"]
     current_directory_path = os.path.dirname(os.path.abspath(__file__))
     level_up_directory_path = "/".join(current_directory_path.split("/")[:-1])
-    with open("/app/.env", "w") as file:
-        file.write(f"SLACK_CHANNEL_ID={channel_id}\n")
+    channels = [channel_id]
+    try:
+        with open(f"{level_up_directory_path}/.env", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                if "SLACK_CHANNEL_ID" in line:
+                    active_channels = ast.literal_eval(line.split("=")[-1][:-1])
+                    if channels[0] not in active_channels:
+                        channels += active_channels
+    except FileNotFoundError:
+        # do nothing if we subscribe for the first time
+        pass
+
+    with open(f"{level_up_directory_path}/.env", "w") as file:
         file.write(f"NOTION_TABLE_URL={notion_table_url}\n")
-        file.write(f"SLACK_CHANNEL_ID={os.getenv('SLACK_CHANNEL_ID')}\n")
+        file.write(f"SLACK_CHANNEL_ID={str(channels)}\n")
         file.write(f"SLACK_SIGNING_SECRET={os.getenv('SLACK_SIGNING_SECRET')}\n")
         file.write(f"SLACK_BOT_TOKEN={os.getenv('SLACK_BOT_TOKEN')}\n")
         file.write(f"CLIENT_ID={os.getenv('CLIENT_ID')}\n")
@@ -116,15 +36,21 @@ def subscribe(ack, body):
         file.write(f"REDIRECT_URI={os.getenv('REDIRECT_URI')}\n")
         file.write(f"NOTION_TOKEN={os.getenv('NOTION_TOKEN')}\n")
         file.write(f"REFRESH_TOKEN={os.getenv('REFRESH_TOKEN')}\n")
-    # subprocess.Popen(
-    #     f"python {level_up_directory_path}/cron-jobs/cron_job_openings.py", shell=True
-    # )
+
+    # docker container binded code should provide full paths to interpreter and executables
+    python_path = "/usr/local/bin/python"
     subprocess.Popen(
-        f"/usr/local/bin/python /app/cron-jobs/cron_job_openings.py",
+        f"{python_path} /app/cron-jobs/cron_job_openings.py",
         shell=True,
     )
-    # setup_cron_jobs(level_up_directory_path)
-    # threading.Thread(target=handle_subscription, args=(notion_table_url,)).start()
+    subprocess.Popen(
+        f"{python_path} /app/cron-jobs/cron_interview_updates.py",
+        shell=True,
+    )
+    subprocess.Popen(
+        f"{python_path} /app/cron-jobs/cron_refresh_token.py",
+        shell=True,
+    )
     ack()
 
 
@@ -134,9 +60,11 @@ def handle_job_openings(ack, body, payload, client):
     if "value" in payload:
         jobs_to_list = ast.literal_eval(payload["value"])
     else:
-        with open("/app/jobs", "r") as file:
+        current_directory_path = os.path.dirname(os.path.abspath(__file__))
+        level_up_directory_path = "/".join(current_directory_path.split("/")[:-1])
+        with open(level_up_directory_path + "/.env", "r") as file:
             lines = file.readlines()
-        jobs = "".join(lines)[5:]
+        jobs = "".join(lines)[5:-1]
         jobs_to_list = ast.literal_eval(jobs)
     client.views_open(
         trigger_id=body["trigger_id"],
