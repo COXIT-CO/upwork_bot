fc3bdeec48046930b6ec50c6ceb3c9591c79e4e9
Vladyslav Snisar <snisarvlad04@gmail.com>
2023-02-22 15:39:32 +0200

    update test functions

--- tests/cron_helpers_test.py
+++ tests/cron_helpers_test.py
@@ -13,14 +13,19 @@ os.environ[
 from app.app import flask_app
 from app.database import DB
 from cron_jobs.helpers import (
+    find_new_job_openings,
+    find_new_invitations,
     remove_job_from_db,
     remove_unactive_jobs_from_db,
-    find_new_job_openings,
+    remove_unactive_invitations_from_db
 )
 from upwork_part.schema.controllers import JobController
+from upwork_part.schema.controllers import InvitationController
 from upwork_part.schema.models import Job as JobModel
+from upwork_part.schema.models import Invitation
 
 job_controller = JobController()
+invitation_controller = InvitationController()
 
 
 def setup_module(module):
@@ -147,7 +152,7 @@ def test_find_new_job_openings_setup():
             1,
             [
                 {
-                    "job_title": "upwork job {i}",
+                    "job_title": f"upwork job {i}",
                     "job_url": f"https://upwork_link_{i}.com",
                 }
                 for i in range(5, 7)
@@ -157,7 +162,7 @@ def test_find_new_job_openings_setup():
             2,
             [
                 {
-                    "job_title": "upwork job {i}",
+                    "job_title": f"upwork job {i}",
                     "job_url": f"https://upwork_link_{i}.com",
                 }
                 for i in range(5)
@@ -167,7 +172,7 @@ def test_find_new_job_openings_setup():
             3,
             [
                 {
-                    "job_title": "upwork job {i}",
+                    "job_title": f"upwork job {i}",
                     "job_url": f"https://upwork_link_{i}.com",
                 }
                 for i in range(3, 7)
@@ -177,7 +182,7 @@ def test_find_new_job_openings_setup():
             4,
             [
                 {
-                    "job_title": "linkedin job {i}",
+                    "job_title": f"linkedin job {i}",
                     "job_url": f"https://linkedin_link_{i}.com",
                 }
                 for i in range(2)
@@ -220,6 +225,111 @@ def test_find_new_job_openings(test_find_new_job_openings_setup, id, test):
                         pytest.fail(f"Job {job_url} isn't found in db!")
 
 
+@pytest.fixture
+def test_find_new_invitations_setup():
+    with flask_app.app_context():
+        for i in range(5):
+            invitation_controller.create(f"https://invitation_{i}.com")
+
+
+@pytest.mark.parametrize(
+    "id,test",
+    [
+        (
+            1,
+            [
+                {"text": f"inv {i}", "link": f"https://invitation_{i}.com"}
+                for i in range(5, 7)
+            ]
+        ),  # all new invitations
+        (
+            2,
+            [
+                {"text": f"inv {i}", "link": f"https://invitation_{i}.com"}
+                for i in range(5)
+            ]
+        ),  # already existing invitations
+        (
+            3,
+            [
+                {"text": f"inv {i}", "link": f"https://invitation_{i}.com"}
+                for i in range(3, 7)
+            ]
+        )  # partially new invitations
+    ]
+)
+def test_find_new_invitations(test_find_new_invitations_setup, id, test):
+    with flask_app.app_context():
+        match id:
+            case 1:
+                find_new_invitations(test)
+                with flask_app.app_context():
+                    for inv_link in [f"https://invitation_{i}.com" for i in range(7)]:
+                        if invitation_controller.get(inv_link) == "":
+                            pytest.fail(f"Invitation {inv_link} isn't found in db!")
+            case 2:
+                find_new_invitations(test)
+                if len(Invitation.query.all()) != 5:
+                    pytest.fail("There must be exactly 5 invitations in db!")
+                for inv_link in [f"https://invitation_{i}.com" for i in range(5)]:
+                    if invitation_controller.get(inv_link) == "":
+                        pytest.fail(f"Invitation {inv_link} isn't found in db!")
+            case 3:
+                find_new_invitations(test)
+                if len(Invitation.query.all()) != 7:
+                    pytest.fail("There must be exactly 7 invitations in db!")
+                for inv_link in [f"https://invitation_{i}.com" for i in range(5)]:
+                    if invitation_controller.get(inv_link) == "":
+                        pytest.fail(f"Invitation {inv_link} isn't found in db!")
+
+
+@pytest.fixture
+def test_remove_unactive_invitations_from_db_setup():
+    with flask_app.app_context():
+        for i in range(5):
+            invitation_controller.create(invitation_url=f"https://invitation_upwork_{i}.com")
+        for i in range(5):
+            invitation_controller.create(invitation_url=f"https://invitation_linkedin_{i}.com")
+        invitation_controller.create(invitation_url=f"https://invitation_tempest_{i}.com")
+
+
+@pytest.mark.parametrize(
+    "id,test",
+    [
+        (1, [{"text": f"inv {i}", "link": f"https://invitation_upwork_{i}.com"} for i in range(5)]),
+        (2, [{"text": "inv", "link": "https://wrong_invitation.com"} for _ in range(5)]),
+        (
+            3,
+            [{"text": f"inv {i}", "link": f"https://invitation_upwork_{i}.com"} for i in range(3)]
+            + [{"text": "inv", "link": "https://invitation_upwork.com"} for _ in range(3)],
+        ),
+    ],
+)
+def test_remove_unactive_invitations_from_db(
+    test_remove_unactive_invitations_from_db_setup, id, test
+):
+    with flask_app.app_context():
+        match id:
+            case 1:
+                remove_unactive_invitations_from_db(test)
+                with flask_app.app_context():
+                    if len(Invitation.query.all()) != 5:
+                        pytest.fail("There must be exactly 5 invitations in db!") 
+            case 2:
+                remove_unactive_invitations_from_db(test)
+                with flask_app.app_context():
+                    if len(Invitation.query.all()) != 0:
+                        pytest.fail("There must no invitations in db!") 
+            case 3:
+                remove_unactive_invitations_from_db(test)
+                with flask_app.app_context():
+                    if len(Invitation.query.all()) != 3:
+                        pytest.fail("There must be exactly 3 invitations in db!")
+                    for inv_url in [f"https://invitation_upwork_{i}.com" for i in range(3)]:
+                        if invitation_controller.get(inv_url) == "":
+                            pytest.fail(f"Invitation {inv_url} must be in db!")
+
+
 def teardown_function():
     """teardown any state that was previously setup in our case with a pytest.fixture
     call.
@@ -227,6 +337,8 @@ def teardown_function():
     with flask_app.app_context():
         for job in JobModel.query.all():
             job_controller.delete(job.job_url)
+        for inv in Invitation.query.all():
+            invitation_controller.delete(inv.url)
 
 
 def teardown_module(module):
