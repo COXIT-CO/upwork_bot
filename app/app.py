3ec2609b0f177d02c4d00e8fdc085182e3676c64
volodymyr hresko <volodymyr.hresko@coxit.co>
2023-02-22 12:53:01 +0200

    update database module to use testing database during testing

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
+import os
 import ssl
 from flask import Flask
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 flask_app = Flask(__name__)
-flask_app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+if os.getenv("TESTING"):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+else:
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
